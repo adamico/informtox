54297c648dba389d4b8d8ac11e3c0471cc981cd8
Andrea D'Amico <andrea.damico@gmail.com>
2012-04-20 15:14:08 +0200

    Created application using Refinery CMS 2.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Informtox::Application.config.secret_token = '1c5b23e8e1ecb4f5c245557d254eb3acf9fcbab8f62b5e78746b5d873d36e0ade00ab4c2c7dd52dac0677382d22121e768cec2e978e17d51a6cf227c71450c8c'
